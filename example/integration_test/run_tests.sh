355ca4752e849ae44c0c38bee663bdb2f011d910
Brian Manuputty <brian.manuputty@wolfpackit.nl>
2022-09-23 22:52:27 +0200

    [HOTFIX] Removed test script

--- example/integration_test/run_tests.sh
+++
@@ -1,9 +0,0 @@
-echo 'flutter: [✉] --- TEST LOG --- [💡] Moving to root..'
-cd ..
-echo 'flutter: [✉] --- TEST LOG --- [💡] Moved to root! Current dir:'
-pwd
-echo 'flutter: [✉] --- TEST LOG --- [💡] Running integration tests..'
-flutter drive \
-  --driver=test_driver/integration_test.dart \
-  --target=integration_test/example_integration_test.dart
-echo 'flutter: [✉] --- TEST LOG --- [💡] Done running integrations tests!'
